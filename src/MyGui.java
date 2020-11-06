21b15df85a20b5dcd4e5a2f81e4e8dc77b49f2e5
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2020-11-06 10:28:18 +0100

    Added MyGui.java

---
+++ src/MyGui.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class MyGui
+{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Happy Coding");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 300);
+        frame.setVisible(true);
+    }
+}
+
