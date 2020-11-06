b812f666ef522812e26d10153ba133ac3cff3d6e
Mads Knudsen <madsfknudsen@hotmail.com>
2020-11-06 09:47:19 +0100

    Added Order Add, Order Remove, Order Total Price

--- src/Order.java
+++ src/Order.java
@@ -1,9 +1,33 @@
 import java.util.ArrayList;
 
 public class Order {
-    public void addOrder(){
+    private ArrayList<Product> list = new ArrayList<>();
 
+    public Product[] getList(){
+        Product[] result = new Product[list.size()];
+
+        for(int i = 0; i < result.length; i++)
+            result[i] = list.get(i);
+
+        return result;
     }
+
+    public void addOrder(Product toAdd){
+        list.add(toAdd);
     }
 
+    public void removeOrder(int index, int amount){
+        Product[] toRemoveList = new Product[amount];
+        int i = 0;
 
+        for (Product product: list) {
+            if (product.getIndex() == index) {
+                toRemoveList[i] = product;
+                i++;
+            }
+        }
+
+        for (i = 0; i < amount; i++)
+            list.remove(toRemoveList[i]);
+    }
+}
