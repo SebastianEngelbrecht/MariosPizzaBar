2703c1d49711411b9da297396c060a2ca1bc7405
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2020-11-24 15:55:52 +0100

    Updated JDBC_DB_Connection.java

--- src/jdbc/JDBC_DB_Connection.java
+++ src/jdbc/JDBC_DB_Connection.java
@@ -19,6 +19,12 @@ public class JDBC_DB_Connection implements AutoCloseable {
     private PreparedStatement ps_create_orderList;
     private PreparedStatement ps_create_pizzaIngredients;
 
+    private PreparedStatement ps_delete_pizzaList;
+
+    private PreparedStatement ps_update_pizzaList;
+
+    private PreparedStatement ps_reorder_pizzaList;
+
 
     public JDBC_DB_Connection(String db_url, String db_user, String db_password) throws SQLException {
         this.db_url = db_url;
@@ -79,6 +85,7 @@ public class JDBC_DB_Connection implements AutoCloseable {
 //            throw new Exception(e);
 //        }
 //    }
+
     public void createPizzaList(String name, float price) throws Exception
     {
         ps_create_pizzaList.setString(1, name);
@@ -86,6 +93,68 @@ public class JDBC_DB_Connection implements AutoCloseable {
         if (ps_create_pizzaList.executeUpdate() != 1)
         {
             throw new Exception("Could not create Pizza");
+        }
+        reorderPizzaListId();
+    }
+
+    public void updatePizzaList(int id ,String name, float price) throws Exception
+    {
+        ps_update_pizzaList.setString(1, name);
+        ps_update_pizzaList.setFloat(2, price);
+        ps_update_pizzaList.setInt(3, id);
+        ps_update_pizzaList.executeUpdate();
+        if (ps_update_pizzaList.executeUpdate() != 1)
+        {
+            throw new Exception("Could not update Pizza");
+        }
+    }
+
+    public void updatePizzaList(int id, String name) throws Exception
+    {
+        float price = getPizzaList().get(id - 1).getPrice();
+        ps_update_pizzaList.setString(1, name);
+        ps_update_pizzaList.setFloat(2, price);
+        ps_update_pizzaList.setInt(3, id);
+        ps_update_pizzaList.executeUpdate();
+        if (ps_update_pizzaList.executeUpdate() != 1)
+        {
+            throw new Exception("Could not update Pizza");
+        }
+    }
+
+    public void updatePizzaList(int id, float price) throws Exception
+    {
+        String name = getPizzaList().get(id - 1).getName();
+        ps_update_pizzaList.setString(1, name);
+        ps_update_pizzaList.setFloat(2, price);
+        ps_update_pizzaList.setInt(3, id);
+        ps_update_pizzaList.executeUpdate();
+        if (ps_update_pizzaList.executeUpdate() != 1)
+        {
+            throw new Exception("Could not update Pizza");
+        }
+    }
+
+    public void deletePizzaList(String name) throws Exception
+    {
+        ps_delete_pizzaList.setString(1, name);
+        if (ps_delete_pizzaList.executeUpdate() != 1)
+        {
+            throw new Exception("Could not delete Pizza");
+        }
+        reorderPizzaListId();
+    }
+
+    public void reorderPizzaListId() throws Exception
+    {
+        try {
+            for (int i = 1; i <= getPizzaList().size(); i++) {
+                ps_reorder_pizzaList.setInt(1, i);
+                ps_reorder_pizzaList.setInt(2, getPizzaList().get(i - 1).getId());
+                ps_reorder_pizzaList.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new Exception(e);
         }
     }
 
@@ -123,9 +192,21 @@ public class JDBC_DB_Connection implements AutoCloseable {
         ps_get_pizzaList = connection.prepareStatement("SELECT * FROM mariospizzabar.pizzalist");
 //        ps_get_pizzaIngredients = connection.prepareStatement("");
 //        ps_get_orderList = connection.prepareStatement("");
+
         ps_create_pizzaList = connection.prepareStatement("INSERT INTO mariospizzabar.pizzalist " +
                 "(Name, Price) VALUES (?,?)");
 //        ps_create_orderList = connection.prepareStatement("");
 //        ps_create_pizzaIngredients = connection.prepareStatement("");
+
+        ps_delete_pizzaList = connection.prepareStatement("DELETE FROM mariospizzabar.pizzalist " +
+                "WHERE Name = ?");
+
+        ps_update_pizzaList = connection.prepareStatement("UPDATE mariospizzabar.pizzalist " +
+                "SET Name = ?, Price = ? " +
+                "WHERE pizzalist_id = ?");
+
+        ps_reorder_pizzaList = connection.prepareStatement("UPDATE mariospizzabar.pizzalist " +
+                "SET pizzalist_id = ? " +
+                "WHERE pizzalist_id = ?");
     }
 }
